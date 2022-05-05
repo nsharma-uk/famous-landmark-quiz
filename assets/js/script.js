7ddad70d4e46c08b6159f27dadb5b320e04135af
nsharma-uk <n.sharmauk711@gmail.com>
2022-05-05 15:32:12 +0100

    linked answer to score

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const questions = [
   {
     questionText: "In which country would you find the Wadi Rum?",
     choices: ["Syria", "Iraq", "Jordan", "Bahrain"],
-    answer: 2, // answer: "Jordan"
+    correctAnswer: "Jordan", // answer: "Jordan"
   },
 
   {
@@ -14,7 +14,7 @@ const questions = [
       "Burj Khalifa",
       "Shanghai Tower",
     ],
-    answer: 3, // answer: Shanghai Tower,
+    correctAnswer: "Shanghai Tower", // answer: Shanghai Tower,
   },
 
   {
@@ -25,26 +25,26 @@ const questions = [
       "Cannonhill Park",
       "Stanley Park",
     ],
-    answer: 1, // answer: Birkenhead
+    correctAnswer: "Birkenhead Park", // answer: Birkenhead
   },
 
   {
     questionText:
       "The World Heritage Site Machu Picchu can be found in which country?",
     choices: ["Ecuador", "Bolivia", "Peru", "Chile"],
-    answer: 2, // answer: Peru
+    correctAnswer: "Peru", // answer: Peru
   },
 
   {
     questionText: "How many countries border the Himalayas?",
     choices: ["4", "5", "6", "7"],
-    answer: 3, // answer: "c",
+    correctAnswer: "6", // answer: "6",
   },
 
   {
     questionText: "How many degrees does the Pisa tower lean?",
     choices: ["5.5 degrees", "6.5 degrees", "7.5 degrees", "8.5 degrees"],
-    answer: 0, //5.5 degrees
+    correctAnswer: "5.5 degrees", //5.5 degrees
   },
 
   {
@@ -56,13 +56,13 @@ const questions = [
       "Around 500 million years",
       "Around 600 million years",
     ],
-    answer: 3, // answer: Around 6oom years
+    correctAnswer: "Around 600 million years", // answer: Around 6oom years
   },
 
   {
     questionText: "How many presidents make up Mount Rushmore?",
     choices: ["3", "4", "5", "6"],
-    answer: 1, // answer: 4
+    correctAnswer: "4", // answer: 4
   },
 ];
 
@@ -88,9 +88,7 @@ let score = 0;
 
 const initialiseLocalStorage = () => {
   // get answer Results from LS
-  const  ResultsFromLS = JSON.parse(
-    localStorage.getItem("answerResults")
-  );
+  const ResultsFromLS = JSON.parse(localStorage.getItem("answerResults"));
 
   const allResultsFromLS = JSON.parse(localStorage.getItem("allResults"));
 
@@ -107,13 +105,100 @@ const initialiseLocalStorage = () => {
 
 const storeInLS = (key, value) => {
   // get feedbackResults from LS
-  const arrayFromLS = JSON.parse(localStorage.getItem(key));
+  let arrayFromLS = JSON.parse(localStorage.getItem(key));
+
+  // if arrayFromLs is not null and of type array only then execute line 113
+  // and operator &&
+  // || OR operator
+  if (arrayFromLS === null) {
+    // create a new array, push your key value to it
+    let myArray = [];
+    myArray.push(value);
+    arrayFromLS = myArray;
+    console.log("arrayFromLS: " + arrayFromLS);
+  } else {
+    arrayFromLS.push(value);
+  }
 
   // push answer in to array
-  arrayFromLS.push(value);
 
   // set feedbackResults in LS
   localStorage.setItem(key, JSON.stringify(arrayFromLS));
+};
+
+const renderForm = () => {
+  //create section
+  const sectionResult = document.createElement("section");
+  sectionResult.setAttribute("class", "results-section");
+  sectionResult.setAttribute("id", "results-section-id");
+
+  //create div
+  const divResultDiv = document.createElement("div");
+  divResultDiv.setAttribute("class", "results-div");
+
+  //create h2
+  const h2Result = document.createElement("h2");
+  h2Result.setAttribute("class", "your-score");
+  h2Result.textContent = "Your Score";
+
+  //paragraphs
+  const pResult1 = document.createElement("p");
+  pResult1.textContent = `Congratulations on completing the quiz! You scored ${score} out of 8`;
+  const pResult2 = document.createElement("p");
+  pResult2.textContent = "You can now see if you are on the leader board";
+
+  //create hr
+  const hrResult = document.createElement("hr");
+  hrResult.setAttribute("class", "results-hr");
+
+  //append child <h2> and <hr> and <p> to parent div
+  divResultDiv.append(h2Result, hrResult, pResult1, pResult2);
+
+  //create form
+  const form = document.createElement("form");
+  form.setAttribute("id", "results-form");
+
+  //create div
+  const divInputName = document.createElement("div");
+  divInputName.setAttribute("id", "div-input-full-name");
+
+  //create input
+  const input = document.createElement("input");
+  input.setAttribute("id", "fname");
+  input.value = "Your name here...";
+
+  ////create div
+  const divSubmit = document.createElement("div");
+  divSubmit.setAttribute("id", "div-submit-to-high-scores");
+
+  //create submit button
+  const submitScoresBtn = document.createElement("button");
+  submitScoresBtn.setAttribute("id", "submit-to-high-scores-button");
+  submitScoresBtn.setAttribute("class", "high-score button");
+  submitScoresBtn.textContent = "Submit your score to the leader board";
+
+  //create div
+  const divViewScores = document.createElement("div");
+  divViewScores.setAttribute("id", "div-view-scores");
+
+  //create view high scores on different page button
+  const viewScoresBtn = document.createElement("button");
+  viewScoresBtn.setAttribute("id", "view-scores-button");
+  viewScoresBtn.setAttribute("class", "view-high-score button");
+  viewScoresBtn.textContent = "View leader board without submitting your score";
+
+  //append children to parents
+  divViewScores.append(viewScoresBtn);
+  divInputName.append(input);
+  divSubmit.append(submitScoresBtn);
+  form.append(divInputName, divSubmit, divViewScores);
+
+  sectionResult.append(divResultDiv, form);
+
+  mainElement.append(sectionResult);
+
+  // add submit event handler to form
+  sectionResult.add;
 };
 
 //event handler for click event for answers
@@ -130,35 +215,54 @@ const handleChoiceClick = (event) => {
     //get the option the user clicked on
     const value = target.getAttribute("data-value");
 
+    console.log(value);
     //get the question the user answered
-    const question = questions[questionIndex].questionText;
+    const correctAnswer = questions[questionIndex].correctAnswer;
 
-    //build an answer object that contains question and answer
-    const answerValue = {
-      question,
-      value,
-    };
+    // compare the 2 answers
+    if (value !== correctAnswer) {
+      console.log("incorrect"); // if incorrect subtract 5 seconds from timerValue
+      timerValue -= 5;
 
-    //store in local storage
-    storeChoicesInLS(answerValue);
+      //TO DO renderQUestionWrongAlert function -> if incorrect render error alert with message and status
+      renderAlert();
+      if (questionIndex < questions.length - 1) {
+        //go to the next question
+        questionIndex += 1;
 
-    //remove both present question and last question
-    removeQuestion();
+        //remove both present question and last question
+        removeQuestion();
 
-    //if it's not the last question
-    if (questionIndex < questions.length - 1) {
-      //go to the next question
-      questionIndex += 1;
-
-      //and render the next question
-      renderQuestion();
+        //and render the next question
+        renderQuestion();
+      } else {
+        //go to the last question amd render  form with scores
+        //remove questions
+        removeQuestion();
+        renderResults();
+        renderForm();
+      }
     } else {
-      //go to the last question amd render form
+      console.log("correct");
 
-      //remove questions
+      score += 1;
+      //if it's not the last question
+      if (questionIndex < questions.length - 1) {
+        //go to the next question
+        questionIndex += 1;
 
-      renderResults();
-      renderForm();
+        //remove both present question and last question
+        removeQuestion();
+
+        //and render the next question
+        renderQuestion();
+      } else {
+        //go to the last question amd render  form with scores
+        //remove questions
+        removeQuestion();
+        renderResults();
+        renderForm();
+      }
     }
   }
 };
@@ -242,6 +346,9 @@ const renderQuestion = () => {
 
   //add event listener to question section
   section.addEventListener("click", handleChoiceClick);
+
+  //TO DO  - Where in renderQuestion do I add this?
+  //section.addEventListener("click", validateAnswer)
 };
 
 //declare function to remove #start-quiz-section from page
@@ -262,6 +369,9 @@ const handleStartButtonClick = () => {
 
 //add event listener to start quiz button
 startButton.addEventListener("click", handleStartButtonClick);
+
+//adding event listener function as a higher order function
+//startButton.addEventListener("click", handleTimerButton);
 
 //start timer
 const startTimer = () => {
@@ -286,22 +396,37 @@ const startTimer = () => {
       }
     }
   };
-  // setInterval of 1000ms (1s)
-  const timerID = setInterval(countdown, 1000);
 };
 
-//adding event listener function as a higher order function
-startButton.addEventListener("click", handleTimerButton);  
-
-const validateAnswer = () => {
+const validateAnswer = (event) => {
   // get answer clicked from user
+  const currentQuestion = questions[questionIndex];
+  const target = event.target;
+
   // get the correct answer for question
+
+  const currentTarget = (event.currentTarget = currentQuestion.correctAnswer);
+
   // compare the 2 answers
-  // if incorrect subtract 5 seconds from timerValue
-  // if incorrect render error alert with message and status
-  // if correct render success alert with message and status
+  if (value !== currentValue) {
+    // if incorrect subtract 5 seconds from timerValue
+    timerValue -= 5;
+
+    //TO DO renderQUestionWrongAlert function -> if incorrect render error alert with message and status
+    renderAlert();
+  } else if (questionIndex < questions.length - 1) {
+    // if question is not last question then increment question index and render next question
+    questionIndex += 1;
+
+    renderQuestion();
+  } else {
+    console.log("hello");
+  }
+
   // set timeout for 500ms and then go to next question
+
   // if question is last question set quizComplete to true and then render form
+
   // if question is not last question then increment question index and render next question
 };
 
@@ -322,82 +447,6 @@ const renderTimerSection = () => {
   // append section to main
 };
 
-const renderForm = () => {
-  //create section
-  const sectionResult = document.createElement("section");
-  sectionResult.setAttribute("class", "results-section");
-  sectionResult.setAttribute("id", "results-section-id");
-
-  //create div
-  const divResultDiv = document.createElement("div");
-  divResultDiv.setAttribute("class", "results-div");
-
-  //create h2
-  const h2Result = document.createElement("h2");
-  h2.setAttribute("class", "your-score");
-  h2.textContent = "Your Score";
-
-  //paragraphs
-  const pResult1 = document.createElement("p");
-  pResult1.textContent =
-    "Congratulations on completing the quiz! You scored ______ out of 8";
-  const pResult2 = document.createElement("p");
-  pResult2.textContent = "You can now see if you are on the leader board";
-
-  //create hr
-  const hrResult = document.createElement("hr");
-  hrResult.setAttribute("class", "results-hr");
-
-  //append child <h2> and <hr> and <p> to parent div
-  divResultDiv.append(h2, hr, p);
-
-  //create form
-  const form = document.createElement("form");
-  form.setAttribute("id", "results-form");
-
-  //create div
-  const divInputName = document.createElement("div");
-  divInputName.setAttribute("id", "div-input-full-name");
-
-  //create input
-  const input = document.createElement("input");
-  input.setAttribute("id", "fname");
-  input.value = "Your name here...";
-
-  ////create div
-  const divSubmit = document.createElement("div");
-  divSubmit.setAttribute("id", "div-input-full-name");
-
-  //create submit button
-  const submitScoresBtn = document.createElement("button");
-  submitScoresBtn.setAttribute("id", "submit-to-high-scores-button");
-  submitScoresBtn.textContent = "Submit your score to the leader board";
-
-  //create div
-  const divViewScores = document.createElement("div");
-  divViewScores.setAttribute("id", "div-view-scores");
-
-  //create view high scores on different page button
-  const viewScoresBtn = document.createElement("button");
-  viewScoresBtn.setAttribute("id", "view-scores-button");
-  viewScoresBtn.textContent =
-    "View high scores table without submitting your answer";
-
-  //append children to parents
-  divViewScores.append(submitScoresBtn);
-  divInputName.append(input);
-  divSubmit.append(submitScoresBtn);
-  form.append(divViewScores, divInputName, divViewScores);
-
-  sectionResult.append(divResultDiv, form);
-
-  mainElement.append(sectionResult);
-  console.log("rendered form");
-
-  // add submit event handler to form
-  sectionResult.add;
-};
-
 const renderGameOver = () => {
   // use HTML as guide and build in JS
   // append section to main
@@ -407,6 +456,7 @@ const renderGameOver = () => {
 const removeQuizPage = () => {
   console.log("remove quiz page");
   removeQuizPage.remove();
+  renderForm();
 };
 
 const renderAlert = (message, status) => {
@@ -417,8 +467,3 @@ const renderAlert = (message, status) => {
 // use HTML as guide and build in JS
 // append section to main
 // add submit event handler to form
-
-const renderQuizCompleteSection = () => {
-  // use HTML as guide and build in JS
-  // append section to main
-};
